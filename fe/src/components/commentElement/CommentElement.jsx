c8302d9857c3d2a65ff0c6d7e6e614b20ef8b117
Marco Santoro <msantor@hotmail.it>
2024-05-05 22:17:55 +0200

    Added comments scss

--- fe/src/components/commentElement/CommentElement.jsx
+++ fe/src/components/commentElement/CommentElement.jsx
@@ -1,111 +1,109 @@
-import React, { useEffect, useState, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Button, Image, Form } from "react-bootstrap";
+import React, { useEffect, useState, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Image, Form } from 'react-bootstrap'
+import { FaPenToSquare, FaXmark, FaTrashCan, FaComment } from 'react-icons/fa6'
+import styles from './CommentElement.module.scss'
 
-import {
-  deleteComment,
-  getComments,
-  updateComment,
-} from "../../redux/reducers/commentsReducer";
-import { getTaskById } from "../../redux/reducers/tasksReducer";
+import { deleteComment, getComments, updateComment } from '../../redux/reducers/commentsReducer'
+import { getTaskById } from '../../redux/reducers/tasksReducer'
 
-const CommentElement = ({ comment, taskObject, loggedUser }) => {
-  const doDispatch = useDispatch();
-  const [dispatch, setDispatch] = useState();
-  const ref = useRef();
+const CommentElement = ({ comment, taskObject, loggedUser, loggedUserId }) => {
+    const doDispatch = useDispatch()
+    const [dispatch, setDispatch] = useState()
+    const ref = useRef()
 
-  const [isAuthor, setIsAuthor] = useState(false);
-  const [isEditingComment, setIsEditingComment] = useState(false);
-  const [commentText, setCommentText] = useState(comment.text);
+    const [isAuthor, setIsAuthor] = useState(false)
+    const [isEditingComment, setIsEditingComment] = useState(false)
+    const [commentText, setCommentText] = useState(comment.text)
 
-  const getFullName = (firstName, lastName) => {
-    return firstName + " " + lastName;
-  };
-
-  const checkCommentAuthorship = () => {
-    loggedUser._id === comment.author._id
-      ? setIsAuthor(true)
-      : setIsAuthor(false);
-  };
-
-  const handleDeleteComment = async (commentId) => {
-    setDispatch(await doDispatch(deleteComment([taskObject._id, commentId])));
-  };
-
-  const toggleEditComment = async () => {
-    setIsEditingComment(!isEditingComment);
-  };
-
-  const onChangeEditComment = (e) => {
-    setCommentText(e.target.value);
-  };
-
-  const handleEditComment = async (e) => {
-    e.preventDefault();
-    setDispatch(await doDispatch(updateComment([comment._id, commentText])));
-    toggleEditComment();
-  };
-
-  useEffect(() => {
-    if (isEditingComment && ref.current) {
-      ref.current.focus();
+    const getFullName = (firstName, lastName) => {
+        return firstName + ' ' + lastName
     }
-  }, [isEditingComment]);
 
-  useEffect(() => {
-    doDispatch(getComments(taskObject._id));
-  }, [dispatch]);
+    const checkCommentAuthorship = () => {
+        if (loggedUserId === comment.author._id) {
+            setIsAuthor(true)
+        } else {
+            setIsAuthor(false)
+        }
+    }
 
-  useEffect(() => {
-    checkCommentAuthorship();
-    doDispatch(getTaskById(taskObject._id));
-  }, [doDispatch]);
+    const handleDeleteComment = async (commentId) => {
+        setDispatch(await doDispatch(deleteComment([taskObject._id, commentId])))
+    }
 
-  return (
-    <div className="d-flex gap-2 mt-3" key={comment._id}>
-      <Image
-        src={comment.author.profileImg}
-        roundedCircle
-        style={{ maxHeight: "35px" }}
-      />
-      <div className="w-100">
-        <div>
-          {getFullName(comment.author.firstName, comment.author.lastName)}
+    const toggleEditComment = async () => {
+        setIsEditingComment(!isEditingComment)
+    }
+
+    const onChangeEditComment = (e) => {
+        setCommentText(e.target.value)
+    }
+
+    const handleEditComment = async (e) => {
+        e.preventDefault()
+        setDispatch(await doDispatch(updateComment([comment._id, commentText])))
+        toggleEditComment()
+    }
+
+    useEffect(() => {
+        if (isEditingComment && ref.current) {
+            ref.current.focus()
+        }
+    }, [isEditingComment])
+
+    useEffect(() => {
+        doDispatch(getComments(taskObject._id))
+    }, [dispatch])
+
+    useEffect(() => {
+        checkCommentAuthorship()
+        doDispatch(getTaskById(taskObject._id))
+    }, [doDispatch])
+
+    return (
+        <div className={styles.comment_container} key={comment._id}>
+            <Image src={comment.author.profileImg} roundedCircle className={styles.image} />
+            <div className="w-100">
+                <div>{getFullName(comment.author.firstName, comment.author.lastName)}</div>
+
+                {isEditingComment ? (
+                    <Form onSubmit={handleEditComment}>
+                        <Form.Control
+                            type="text"
+                            disabled={!isEditingComment}
+                            ref={ref}
+                            onChange={onChangeEditComment}
+                            value={commentText}
+                        />
+                    </Form>
+                ) : (
+                    <div className={styles.comment}>{commentText}</div>
+                )}
+
+                {isAuthor &&
+                    (isEditingComment ? (
+                        <div className={styles.btn_container}>
+                            <div className={styles.btn_save} onClick={handleEditComment}>
+                                <FaComment />
+                            </div>
+                            <div className={styles.btn_discard} onClick={toggleEditComment}>
+                                <FaXmark />
+                            </div>
+                        </div>
+                    ) : (
+                        <div className={styles.btn_container}>
+                            <div className={styles.btn_save} onClick={toggleEditComment}>
+                                <FaPenToSquare />
+                            </div>
+                            <div className={styles.btn_discard} onClick={() => handleDeleteComment(comment._id)}>
+                                <FaTrashCan />
+                            </div>
+                        </div>
+                    ))}
+            </div>
         </div>
-        <Form onSubmit={handleEditComment}>
-          <Form.Control
-            type="text"
-            disabled={!isEditingComment}
-            ref={ref}
-            onChange={onChangeEditComment}
-            value={commentText}
-          />
-        </Form>
+    )
+}
 
-        {isAuthor ? (
-          isEditingComment ? (
-            <div className="d-flex gap-2">
-              <a href="#" onClick={handleEditComment}>
-                Save
-              </a>
-              <a href="#" onClick={toggleEditComment}>
-                Discard changes
-              </a>
-            </div>
-          ) : (
-            <div className="d-flex gap-2">
-              <a href="#" onClick={toggleEditComment}>
-                Edit
-              </a>
-              <a href="#" onClick={() => handleDeleteComment(comment._id)}>
-                Delete
-              </a>
-            </div>
-          )
-        ) : null}
-      </div>
-    </div>
-  );
-};
-
-export default CommentElement;
+export default CommentElement
--- fe/src/components/commentsSection/CommentsSection.jsx
+++ fe/src/components/commentsSection/CommentsSection.jsx
@@ -1,99 +1,92 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { jwtDecode } from "jwt-decode";
-import { Button, Image, Form } from "react-bootstrap";
-import CommentElement from "../commentElement/CommentElement";
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { jwtDecode } from 'jwt-decode'
+import { Button, Image, Form } from 'react-bootstrap'
+import styles from './CommentsSection.module.scss'
+import { FaFloppyDisk, FaXmark, FaComment, FaComments } from 'react-icons/fa6'
+import CommentElement from '../commentElement/CommentElement'
 
-import { getUserById } from "../../redux/reducers/usersReducer";
-import {
-  createComment,
-  getComments,
-} from "../../redux/reducers/commentsReducer";
-import { getTaskById } from "../../redux/reducers/tasksReducer";
+import { getUserById } from '../../redux/reducers/usersReducer'
+import { createComment, getComments } from '../../redux/reducers/commentsReducer'
+import { getTaskById } from '../../redux/reducers/tasksReducer'
 
 const CommentsSection = ({ taskObject }) => {
-  const doDispatch = useDispatch();
-  const [dispatch, setDispatch] = useState();
-  const comments = useSelector((state) => state.getComments.comments);
-  const userId = jwtDecode(useSelector((state) => state.login.token)).userId;
-  const user = useSelector((state) => state.getUserById.user);
+    const doDispatch = useDispatch()
+    const [dispatch, setDispatch] = useState()
+    const comments = useSelector((state) => state.getComments.comments)
+    const userId = jwtDecode(useSelector((state) => state.login.token)).userId
+    const user = useSelector((state) => state.getUserById.user)
 
-  const [isCreateComment, setIsCreateComment] = useState(false);
-  const [newComment, setNewComment] = useState("");
+    const [isCreateComment, setIsCreateComment] = useState(false)
+    const [newComment, setNewComment] = useState('')
 
-  const toggleCreateComment = () => {
-    setIsCreateComment(!isCreateComment);
-  };
+    const toggleCreateComment = () => {
+        setIsCreateComment(!isCreateComment)
+    }
 
-  const onChangeCreateCommentInput = (e) => {
-    setNewComment(e.target.value);
-  };
+    const onChangeCreateCommentInput = (e) => {
+        setNewComment(e.target.value)
+    }
 
-  const handleCreateComment = async (e) => {
-    e.preventDefault();
-    setDispatch(await doDispatch(createComment([taskObject._id, newComment])));
-    setNewComment("");
-    toggleCreateComment();
-  };
+    const handleCreateComment = async (e) => {
+        e.preventDefault()
+        setDispatch(await doDispatch(createComment([taskObject._id, newComment])))
+        setNewComment('')
+        toggleCreateComment()
+    }
 
-  useEffect(() => {
-    doDispatch(getTaskById(taskObject._id));
-    doDispatch(getComments(taskObject._id));
-  }, [dispatch]);
+    useEffect(() => {
+        doDispatch(getTaskById(taskObject._id))
+        doDispatch(getComments(taskObject._id))
+    }, [dispatch])
 
-  useEffect(() => {
-    doDispatch(getUserById(userId));
-  }, [doDispatch]);
+    useEffect(() => {
+        doDispatch(getUserById(userId))
+    }, [doDispatch])
 
-  return (
-    <>
-      <div className="mt-4 fs-4">Comments</div>
-      <div className="d-flex gap-2 mt-2">
-        <Image
-          src={user.profileImg}
-          roundedCircle
-          style={{ maxHeight: "35px" }}
-        />
-        {isCreateComment ? (
-          <div className="mt-2 w-100">
-            <Form onSubmit={handleCreateComment}>
-              <Form.Control
-                type="text"
-                autoFocus
-                placeholder="Write a comment..."
-                onChange={onChangeCreateCommentInput}
-              />
-            </Form>
-            <div className="d-flex gap-2 mt-2">
-              <Button variant="success" onClick={handleCreateComment}>
-                Save
-              </Button>
-              <Button variant="danger" onClick={toggleCreateComment}>
-                Cancel
-              </Button>
+    return (
+        <>
+            <div className={styles.title}>
+                <FaComments className={styles.commentsIcon} />
+                Comments
             </div>
-          </div>
-        ) : (
-          <Button className="w-100 text-start" onClick={toggleCreateComment}>
-            Write a comment...
-          </Button>
-        )}
-      </div>
+            <div className={styles.body}>
+                <Image src={user.profileImg} roundedCircle className={styles.image} />
+                {isCreateComment ? (
+                    <div className="w-100">
+                        <Form onSubmit={handleCreateComment}>
+                            <Form.Control
+                                type="text"
+                                autoFocus
+                                placeholder="Write a comment..."
+                                onChange={onChangeCreateCommentInput}
+                            />
+                        </Form>
+                        <div className={styles.btn_container}>
+                            <div className={styles.btn_save} onClick={handleCreateComment}>
+                                <FaComment />
+                            </div>
+                            <div className={styles.btn_discard} onClick={toggleCreateComment}>
+                                <FaXmark />
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className={styles.btn_createComment} onClick={toggleCreateComment}>
+                        Write a comment...
+                    </div>
+                )}
+            </div>
 
-      {comments ? (
-        <div className="mt-3">
-          {comments.map((comment) => (
-            <CommentElement
-              key={comment._id}
-              comment={comment}
-              taskObject={taskObject}
-              loggedUser={user}
-            />
-          ))}
-        </div>
-      ) : null}
-    </>
-  );
-};
+            {comments && (
+                <div className="mt-3">
+                    {comments.map((comment) => (
+                        <CommentElement key={comment._id} comment={comment} taskObject={taskObject} loggedUser={user} loggedUserId={userId}/>
+                    ))}
+                </div>
+            )}
+        </>
+    )
+}
 
-export default CommentsSection;
+export default CommentsSection
