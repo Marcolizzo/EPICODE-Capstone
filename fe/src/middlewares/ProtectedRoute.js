3a102be03beef75e97744fd2e1150b345f63914d
Marco Santoro <91908791+Marcolizzo@users.noreply.github.com>
2024-05-06 14:26:07 +0200

    Update ProtectedRoutes.js

--- fe/src/middlewares/ProtectedRoute.js
+++ fe/src/middlewares/ProtectedRoute.js
@@ -1,21 +1,31 @@
-import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
 
 const useAuth = () => {
-    return JSON.parse(localStorage.getItem('auth'));
+    // return JSON.parse(localStorage.getItem('auth'));
+    const token = JSON.parse(localStorage.getItem('auth'))
+    const currentTime = Date.now() / 1000
+
+    if (!token) {
+        return false
+    }
+
+    const decodedToken = jwtDecode(token)
+    return decodedToken.exp > currentTime
 }
 
 const ProtectedRoutes = () => {
-    const isAuth = useAuth();
-    const navigate = useNavigate();
+    const isAuth = useAuth()
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (!isAuth) {
-            navigate("/login");
+            navigate('/login')
         }
-    }, [isAuth, navigate]);
+    }, [isAuth, navigate])
 
-    return isAuth ? <Outlet /> : null;
+    return isAuth ? <Outlet /> : null
 }
 
-export default ProtectedRoutes;
+export default ProtectedRoutes
