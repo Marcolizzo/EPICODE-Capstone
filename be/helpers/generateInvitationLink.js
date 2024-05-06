67054a8639cf3f6c19a674b7d36c85ab4494d0ae
Marco Santoro <msantor@hotmail.it>
2024-05-06 19:31:50 +0200

    Update helpers

--- be/helpers/generateInvitationLink.js
+++ be/helpers/generateInvitationLink.js
@@ -1,19 +1,19 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const generateInvitationLink = (projectId, inviterId) => {
-  // Define the payload for the JWT token
-  const payload = {
-    projectId: projectId,
-    inviterId: inviterId,
-  };
+// const generateInvitationLink = (projectId, inviterId) => {
+//   // Define the payload for the JWT token
+//   const payload = {
+//     projectId: projectId,
+//     inviterId: inviterId,
+//   };
 
-  // Generate the JWT token with a secret key
-  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
+//   // Generate the JWT token with a secret key
+//   const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
 
-  // Create the invitation link
-  const invitationLink = `${process.env.BASE_URL}/login?token=${token}`;
+//   // Create the invitation link
+//   const invitationLink = `${process.env.BASE_URL}/login?token=${token}`;
 
-  return invitationLink;
-};
+//   return invitationLink;
+// };
 
-module.exports = { generateInvitationLink };
+// module.exports = { generateInvitationLink };
